1b44215b006ba4ba1be5350c3a280477febacf18
SougataMondal2000 <sougataofficial362000@gmail.com>
2024-12-14 13:21:06 +0530

    fix

--- src/app/baseURL.js
+++ src/app/baseURL.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:5000";
-// export const baseURL = "https://starfish-app-fko8w.ondigitalocean.app";
+// export const baseURL = "http://localhost:5000";
+export const baseURL = "https://starfish-app-fko8w.ondigitalocean.app";
