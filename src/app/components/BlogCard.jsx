862daae926ffb3550ddf2c9ebc38a01f3c73a5cf
SamratPaul2000 <samratpaul5001x@gmail.com>
2025-01-10 13:22:50 +0530

    fix

--- src/app/components/BlogCard.jsx
+++ src/app/components/BlogCard.jsx
@@ -9,28 +9,28 @@ const BlogCard = ({
   redirectTo,
 }) => {
   return (
-    <div className="bg-cream rounded-2xl shadow-xl p-8 min-h-[580px] relative size-auto">
+    <div className="bg-cream rounded-3xl p-4 relative size-auto border border-red-100 ">
       {/* Image container with hover effects */}
-      <div className="relative mb-8 group">
+      <div className="relative mb-4 group">
         {/* Background effect that only appears on hover */}
-        <div className="absolute -right-2 -top-2 w-full h-64 bg-orange-300 rounded-xl transform rotate-2 transition-all duration-300 opacity-0 scale-95 group-hover:opacity-100 group-hover:scale-105 group-hover:rotate-3"></div>
+        <div className="absolute -right-2 -top-2 w-full h-40 bg-orange-300 rounded-xl transform rotate-2 transition-all duration-300 opacity-0 scale-85 group-hover:opacity-100 group-hover:scale-105 group-hover:rotate-3"></div>
         <img
           src={imgUrl}
           alt="Helping to Navigate"
-          className="relative w-full h-64 object-cover rounded-xl shadow-md transition-all duration-300 group-hover:scale-105 group-hover:shadow-xl"
+          className="relative w-full h-56 object-cover rounded-xl shadow-md transition-all duration-300 group-hover:scale-105 group-hover:shadow-xl"
         />
       </div>
-      <div className="px-2 bg-transparent">
-        <div className="mb-4">
-          <h2 className="text-xl font-bold border-l-4 border-amber-800 pl-4 py-2 bg-amber-100 rounded-r-xl">
+      <div className="px-1 bg-transparent">
+        <div className="">
+          <h2 className="text-xl font-semibold border-l-4 border-amber-800 pl-4 py-2 bg-amber-100 rounded-r-xl">
             {title}
           </h2>
         </div>
-        <p className="text-gray-600 mt-4 mb-4 text-base leading-relaxed line-clamp-2">
+        <p className="text-gray-600 mt-4 mb-6 text-base leading-relaxed ">
           {description}
         </p>
       </div>
-      <div className="absolute bottom-5 left-5 right-5 my-6">
+      <div className="relative bottom-2 left-1 px-4">
         <a
           href={redirectTo}
           className="flex items-center justify-center gap-2 py-2 bg-amber-500 text-white font-bold rounded-xl hover:bg-amber-600 transition-all duration-300 shadow-md hover:shadow-lg"
